ebc5b1c68e70477f7278905ffd2d5115f2a06c0e
bangen.wang <bangen.wang@lifesense.com>
2019-11-28 16:47:21 +0800

    Basic operations: Create table, Insert Data, View Data, Update Data, Delete Data

--- app/src/main/java/com/example/sqlitedatabasetesting/MainActivity.java
+++ app/src/main/java/com/example/sqlitedatabasetesting/MainActivity.java
@@ -1,21 +1,144 @@
 package com.example.sqlitedatabasetesting;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sqlitedatabasetesting.SQLite.DatabaseHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     DatabaseHelper myDB;
+
+    EditText etName, etSurname, etMarks, etId;
+    Button btnAddData, btnViewAll, btnUpdate, btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etName = findViewById(R.id.etName);
+        etSurname = findViewById(R.id.etSurname);
+        etMarks = findViewById(R.id.etMarks);
+        etId = findViewById(R.id.etId);
+        btnAddData = findViewById(R.id.button_add);
+        btnViewAll = findViewById(R.id.button_viewAll);
+        btnUpdate = findViewById(R.id.button_update);
+        btnDelete = findViewById(R.id.button_delete);
+
+        btnAddData.setOnClickListener(this);
+        btnViewAll.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+
+
         myDB = new DatabaseHelper(this);
 
+
+
     }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.button_add:
+                AddData();
+                break;
+
+
+            case R.id.button_viewAll:
+                viewData();
+                break;
+
+            case R.id.button_update:
+                updateData();
+                break;
+
+            case R.id.button_delete:
+                deleteData();
+                break;
+        }
+    }
+
+
+    public void AddData(){
+        boolean isInserted = myDB.insertData(etName.getText().toString(), etSurname.getText().toString(), Integer.parseInt(etMarks.getText().toString()));
+
+        if (isInserted){
+            Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Data Inserted Failed", Toast.LENGTH_SHORT).show();
+        }
+
+        Toast.makeText(this, etName.getText().toString()+" "+etSurname.getText().toString()+" "+etMarks.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    public void viewData(){
+        Cursor res = myDB.getAllData();
+
+        if (res.getCount() == 0){
+            Toast.makeText(this, "No Available Data", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            StringBuffer buffer = new StringBuffer();
+
+            while (res.moveToNext()){
+                buffer.append("ID: " + res.getString(0) + "\nName: "+ res.getString(1) + "\nSurname: " + res.getString(2) + "\nMarks: " + res.getString(3) + "\n\n");
+            }
+
+            showMessage("Data", buffer.toString());
+
+
+
+        }
+    }
+
+
+    public void updateData(){
+
+        boolean isUpdate = myDB.updateData(etId.getText().toString(), etName.getText().toString(), etSurname.getText().toString(), Integer.parseInt(etMarks.getText().toString()));
+
+        if (isUpdate){
+            showMessage("Update", "Updated Successfully");
+        }
+        else{
+            showMessage("Update", "Updated Failed");
+        }
+
+    }
+
+    public void deleteData(){
+        Integer deletedRows = myDB.deleteData(etId.getText().toString());
+        if (deletedRows>0){
+            showMessage("Delete", "Deleted Successfully");
+        }
+        else{
+            showMessage("Delete", "Deleted Failed");
+        }
+    }
+
+
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+
+    }
+
 }
--- app/src/main/java/com/example/sqlitedatabasetesting/SQLite/DatabaseHelper.java
+++ app/src/main/java/com/example/sqlitedatabasetesting/SQLite/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.sqlitedatabasetesting.SQLite;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -18,12 +20,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+//        SQLiteDatabase db = this.getWritableDatabase();
+
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE table "+TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAE TEXT, MARKS INTEGER)");
+        db.execSQL("CREATE table IF NOT EXISTS "+TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
 
     }
 
@@ -32,4 +36,45 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
     }
+
+
+    public boolean insertData(String name, String surname, int marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+        return db.insert(TABLE_NAME, null, contentValues) !=-1 ? true : false;
+
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        return res;
+
+    }
+
+
+    public boolean updateData(String id, String name, String surname, int marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+        return db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{id}) !=0 ? true : false;
+
+
+    }
+
+
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
+    }
+
+
 }
